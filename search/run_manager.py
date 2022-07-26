c7158f031f5c59a1dd06eeef721fc7aa289c9a8f
bryanbits <70453261+bryanbits@users.noreply.github.com>
2022-07-25 18:22:58 -0700

    replace '/' with '\'

--- search/run_manager.py
+++ search/run_manager.py
@@ -579,7 +579,7 @@ class RunManager:
                         'Time after lr update {lr_adjustment_time.val:.3f} ({lr_adjustment_time.avg:.3f})\t' \
                         'Time after accuracy updates {accuracy_update_time.val:.3f} ({accuracy_update_time.avg:.3f})\t' \
                         'Time after computation of output {output_computation_time.val:.3f} ({output_computation_time.val:.3f})\t' \
-                        'Time after backpropagation {backprop_time.val:.3f} ({backprop_time.avg:.3f})'. /
+                        'Time after backpropagation {backprop_time.val:.3f} ({backprop_time.avg:.3f})'. \
             format(epoch_ + 1, i, nBatch - 1, batch_time=batch_time, data_time=data_time, losses=losses, top1=top1)
             
             if print_top5:
